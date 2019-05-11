2deb874e2e34f83ab4c40d9b793bdd6d8aadc389
ShakedZrihen <shaked.zrihen@gmail.com>
2019-05-11 16:42:44 +0300

    Remove undo and redo buttons

--- paintApp.js
+++ paintApp.js
@@ -8,5 +8,3 @@ paint.init();
 paint.toolbar.addButton(new LineBtn("lineBtn", paint));
 paint.toolbar.addButton(new CircleBtn("circleBtn", paint));
 paint.toolbar.addButton(new BezierBtn("bezierBtn", paint));
-paint.toolbar.addButton(new UndoBtn("undoBtn", paint));
-paint.toolbar.addButton(new RedoBtn("redoBtn", paint));
