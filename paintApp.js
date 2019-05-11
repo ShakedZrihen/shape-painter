880a3e0a61e5cf1cfea2b59a34e9f18660e16725
ShakedZrihen <shaked.zrihen@gmail.com>
2019-05-11 20:18:20 +0300

    Add import button

--- paintApp.js
+++ paintApp.js
@@ -9,3 +9,4 @@ paint.toolbar.addButton(new LineBtn("lineBtn", paint));
 paint.toolbar.addButton(new CircleBtn("circleBtn", paint));
 paint.toolbar.addButton(new BezierBtn("bezierBtn", paint));
 paint.toolbar.addButton(new ExportBtn("exportBtn", paint));
+paint.toolbar.addButton(new ImportBtn("importBtn", paint));
