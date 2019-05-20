8e72b9da3ceefc1335e312196bb8619c2488ba27
ShakedZrihen <shaked.zrihen@gmail.com>
2019-05-20 12:58:43 +0300

    Implement scale functionallity

--- components/toolbar/buttons/bezier_button.js
+++ components/toolbar/buttons/bezier_button.js
@@ -8,7 +8,7 @@ class BezierBtn extends ShapeButtonBase {
   }
 
   handleButtonPress(event) {
-    const numberOfLines = this.getNumberOfLines();
+    const numberOfLines = 100;
     this.paint.canvas.setContext(
       new BezierCurve(this.paint.canvas, [], numberOfLines)
     );
@@ -18,19 +18,7 @@ class BezierBtn extends ShapeButtonBase {
     this.btn.style.backgroundColor = "";
   }
 
-  getNumberOfLines() {
-    const numberOfLines = document.getElementById("number-of-lines").value;
-    if (numberOfLines) {
-      return numberOfLines;
-    }
-    return 100; // Return default number of lines
-  }
-
   select() {
     this.btn.style.backgroundColor = "#282828";
-    const setBtn = document.getElementById("set-number-of-lines-btn");
-    setBtn.addEventListener("click", event => {
-      this.handleButtonPress(event);
-    });
   }
 }
--- components/toolbar/buttons/scale_button.js
+++ components/toolbar/buttons/scale_button.js
@@ -10,30 +10,43 @@ class ScaleBtn extends ButtonBase {
   handleButtonPress(event) {
     this.paint.toolbar.clearSelected();
     this.select();
-    const REPLACE_WITH_SCALE_SIZE = 0.5;
-    for (let i = 0; i < this.paint.canvas.storedSapes.length; ++i) {
-      let currentShape = this.paint.canvas.storedSapes[i];
-      if (currentShape.type() === "Circle") {
-        currentShape.radius = currentShape.radius * REPLACE_WITH_SCALE_SIZE;
-      }
-    }
-    for (let i = 0; i < this.paint.canvas.points.length; ++i) {
-      const point = this.paint.canvas.points[i];
-      point.x = point.x * REPLACE_WITH_SCALE_SIZE;
-      point.y = point.y * REPLACE_WITH_SCALE_SIZE;
-    }
-    this.paint.canvas.redrawStoredShapes();
+    this.paint.canvas.clearListeners();
+    const setterBtn = document.getElementById("setter-btn");
+    setterBtn.addEventListener("click", event =>
+      this.rescale(this.paint.canvas)
+    );
+    setterBtn.innerHTML = "scale";
+    document.getElementById("setter-value").value = 1.0;
   }
 
-  rescale() {}
+  rescale(canvas) {
+    canvas.update();
+    const scaleRasio = document.getElementById("setter-value").value;
+    if (isNaN(scaleRasio) || scaleRasio <= 0) {
+      alert("Must input a positive number");
+      return;
+    }
+    for (let i = 0; i < canvas.storedSapes.length; ++i) {
+      let currentShape = canvas.storedSapes[i];
+      if (currentShape.type() === "Circle") {
+        currentShape.radius = currentShape.radius * scaleRasio;
+      }
+    }
+    for (let i = 0; i < canvas.points.length; ++i) {
+      const point = canvas.points[i];
+      point.x = point.x * scaleRasio;
+      point.y = point.y * scaleRasio;
+    }
+    canvas.redrawStoredShapes();
+  }
 
   clearSelect() {
     this.btn.style.backgroundColor = "";
-    document.getElementById("set-number-of-lines").style.display = "none";
+    document.getElementById("setter").style.display = "none";
   }
 
   select() {
     this.btn.style.backgroundColor = "#282828";
-    document.getElementById("set-number-of-lines").style.display = "block";
+    document.getElementById("setter").style.display = "block";
   }
 }
