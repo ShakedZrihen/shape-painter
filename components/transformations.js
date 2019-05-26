5a6849f783d344daee138f7141ef1b9c7aab26b0
ShakedZrihen <shaked.zrihen@gmail.com>
2019-05-26 17:09:15 +0300

    Add comments

--- components/transformations.js
+++ components/transformations.js
@@ -3,16 +3,26 @@
  */
 
 class Transform {
+  /**
+   * Change object size according to x, y ratio.
+   * the function scales the objects relative to Point(0,0)
+   * @param canvas - the canvas where the object is on it
+   * @param ratioX - ratio to resize all x points
+   * @param ratioY - ratio to resize all y points (default is null for fixed shapes (scale x and y in the       same ratio))
+   * @param updateCanvas - parameters that tells if we need to update canvas
+   */
   static scale(canvas, ratioX, ratioY = null, updateCanvas = true) {
     const center = canvas.calculateCenter();
     ratioY = ratioY === null ? ratioX : ratioY;
     Transform.move(canvas, center, new Point(0, 0), false);
+    // if the shape is Circle -> we need to resize the raius also
     for (let i = 0; i < canvas.storedSapes.length; ++i) {
       let currentShape = canvas.storedSapes[i];
       if (currentShape.type() === "Circle") {
         currentShape.radius = currentShape.radius * ratioX;
       }
     }
+    // for all shapes (include the circles) we need to resize their points
     for (let i = 0; i < canvas.points.length; ++i) {
       const point = canvas.points[i];
       point.x = point.x * ratioX;
@@ -24,13 +34,22 @@ class Transform {
     }
   }
 
+  /**
+   * the function rotate the object clockwise in angle's angle
+   * @param canvas - the canvas where the object is on it
+   * @param centerPoint - center of the object
+   * @param angle (in radians)
+   * @param updateCanvas - parameters that tells if we need to update canvas
+   */
   static rotate(canvas, centerPoint, angle, updateCanvas = true) {
+    // save the first center because it can change while we change the points
     const tempCenter = centerPoint;
     const cosA = Math.cos(angle);
     const sinA = Math.sin(angle);
     Transform.move(canvas, centerPoint, new Point(0, 0), false);
     for (let i = 0; i < canvas.points.length; ++i) {
       const point = canvas.points[i];
+      // save first values
       const tempx = point.x;
       const tempy = point.y;
       point.x = tempx * cosA - tempy * sinA;
@@ -41,6 +60,7 @@ class Transform {
       canvas.update();
     }
   }
+
   static move(canvas, firstPoint, secondPoint, updateCanvas = true) {
     const aX = secondPoint.x - firstPoint.x;
     const aY = secondPoint.y - firstPoint.y;
@@ -58,6 +78,13 @@ class Transform {
     }
   }
 
+  /**
+   * Shear on X axis according to assignment's requirement
+   * @param canvas
+   * @param firstPoint
+   * @param secondPoint
+   * @param updateCanvas
+   */
   static shearing(canvas, firstPoint, secondPoint, updateCanvas = true) {
     const dx = -1 * (secondPoint.x - firstPoint.x);
     const max = calculateMaxAndMinPointsInArray(canvas.points)[MAX];
@@ -103,6 +130,7 @@ class Transform {
     }
   }
 
+  // fit points to canvas size (window to viewport transformation)
   static fit(canvas, updateCanvas = false) {
     let maxMinPoints = calculateMaxAndMinPointsInArray(canvas.points);
     Transform.moveBy(
